53de9cdaaa275d1e130ba12a5cb0204545b6e558
Narges <nargesirezaei@gmail.com>
2025-11-09 19:01:26 +0100

    creating new page for admin to add a new tag.Add project files.

---
+++ Controllers/AdminTagsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggie.web.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult SubmittTag()
+        {
+            var name = Request.Form["name"];
+            var displayName = Request.Form["displayName"];
+            return View("Add");
+        }
+
+    }
+}
