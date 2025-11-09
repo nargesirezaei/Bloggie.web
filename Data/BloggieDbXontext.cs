d9a3c582537c12f2133263b28aa728ffe09ba206
Narges <nargesirezaei@gmail.com>
2025-11-09 19:01:26 +0100

    Add project files.

---
+++ Data/BloggieDbXontext.cs
@@ -0,0 +1,19 @@
+﻿using Bloggie.web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bloggie.web.Data
+{
+    public class BloggieDbXontext : DbContext
+    {
+        public BloggieDbXontext(DbContextOptions options) : base(options)
+        {
+            //later on overwrite these DbContextOptions from program.cs. and we want to pass these options to the base class and
+            ////hence we are creating this constructor with dbContext options parameters 
+
+        }
+
+        public DbSet<Tag> tags { get; set; }
+        public DbSet<BlogPost> BloggPosts { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+using Bloggie.web.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+//inject DbContext inside the services of our application
+builder.Services.AddDbContext<BloggieDbXontext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("BloggieDbConnectionString")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
