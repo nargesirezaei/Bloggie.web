a00648a1061885e4df5c873e984deab2e292c4af
Narges <nargesirezaei@gmail.com>
2025-12-26 13:59:17 +0100

    Added admin user management features: - Display list of all registered users in admin panel - Enabled delete functionality for selected users - Improved view and controller logic for user handling

--- Controllers/AdminUserController.cs
+++ Controllers/AdminUserController.cs
@@ -2,40 +2,99 @@
 using Bloggie.web.Models.ViewModels;
 using Bloggie.web.Repository;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
 namespace Bloggie.web.Controllers
 {
-    [Authorize(Roles ="Admin")]
+    [Authorize(Roles = "Admin")]
     public class AdminUserController : Controller
     {
         private readonly IUserRepository userRepository;
-        public AdminUserController(IUserRepository userRepository)
+        private readonly UserManager<IdentityUser> userManager;
+        public AdminUserController(IUserRepository userRepository,
+                                    UserManager<IdentityUser> userManager)
         {
             this.userRepository = userRepository;
+            this.userManager = userManager;
+
         }
 
         public async Task<IActionResult> List()
         {
-            var users = await userRepository.GetAll();
+            var users = await 
+                userRepository.GetAll();
 
             var usersViewModel = new UserViewModel();
             usersViewModel.Users = new List<User>();
 
 
-            foreach(var user in users)
+            foreach (var user in users)
             {
                 usersViewModel.Users.Add(new Models.ViewModels.User
                 {
                     Id = Guid.Parse(user.Id),
-                    Username =user.UserName,
+                    Username = user.UserName,
                     Email = user.Email
 
                 });
             }
-            
+
             return View(usersViewModel);
+        }
+        [HttpPost]
+        public async Task<IActionResult> List(UserViewModel request)
+        {
+            var identityUser = new IdentityUser
+            {
+                UserName = request.Username,
+                Email = request.Email
+            };
+
+            var identityResult =
+                await userManager.CreateAsync(identityUser, request.Password);
+
+            if (identityResult is not null)
+            {
+                if (identityResult.Succeeded)
+                {
+                    //assign roles to this user 
+                    var roles = new List<string> { "User" };
+
+                    if (request.AdminRoleCheckBox)
+                    {
+                        roles.Add("Admin");
+                    }
+
+                    identityResult =
+                       await userManager.AddToRolesAsync(identityUser, roles);
+
+
+
+                    if(identityResult is not null && identityResult.Succeeded)
+                    {
+                        return RedirectToAction("List", "AdminUser");
+                    }
+                }
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var user = await userManager.FindByIdAsync(id.ToString());
+            if(user != null)
+            {
+                var identityResult = await userManager.DeleteAsync(user);
+
+                if(identityResult != null && identityResult.Succeeded)
+                {
+                    return RedirectToAction("List", "AdminUser");
+                }
+            }
+            return View();
         }
     }
 }
